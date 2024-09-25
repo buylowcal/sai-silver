c25be60d772d8bd1f4b081bbeff569c8699f2296
vinender <vinendersingh91@gmail.com>
2024-09-25 12:11:53 +0530

    product grid spacing added

--- store/src/components/landingPage/featured-products.jsx
+++ store/src/components/landingPage/featured-products.jsx
@@ -16,7 +16,7 @@ function FeaturedProducts({popularProducts,attributes}) {
     return (
       <section id="featured" className="py-12  bg-gray-50">
         <h2 className="text-3xl  tracking-widest text-center mb-8">Featured Products</h2>
-        <div className="  mx-auto px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-y-20  ">
+        <div className="  mx-auto px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-y-20 gap-8  ">
           {popularProducts
                           ?.slice(
                             0,
