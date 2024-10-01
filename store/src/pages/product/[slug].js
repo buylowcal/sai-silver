8aa1fbc9613742b1358f67b153732affcece9aec
vinender <vinendersingh91@gmail.com>
2024-10-01 10:57:38 +0530

    margin added on  product page breadcrumb

--- store/src/pages/product/[slug].js
+++ store/src/pages/product/[slug].js
@@ -241,7 +241,7 @@ const ProductScreen = ({ product, attributes, relatedProducts }) => {
         >
           <div className="px-0 py-10 lg:py-10">
             <div className="mx-auto px-3 lg:px-10 max-w-screen-2xl">
-              <div className="flex items-center pb-4">
+              <div className="flex items-center mt-12 pb-4">
                 <ol className="flex items-center w-full overflow-hidden font-serif">
                   <li className="text-sm pr-1 transition duration-200 ease-in cursor-pointer hover:text-emerald-500 font-semibold">
                     <Link href="/">Home</Link>
