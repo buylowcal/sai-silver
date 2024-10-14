0e5c8e3755b54e5aca5308f667fc97adac13a48c
Bavneet Kaur <bavneetgulab@gmail.com>
2024-10-14 16:32:07 +0530

    updated the change password functionality tested validations

--- store/src/pages/user/change-password.js
+++ store/src/pages/user/change-password.js
@@ -66,9 +66,10 @@ const ChangePassword = () => {
                 <div className="col-span-6 sm:col-span-6">
                   <InputArea
                     register={register}
-                    label={showingTranslateValue(
-                      storeCustomizationSetting?.dashboard?.user_email
-                    )}
+                    // label={showingTranslateValue(
+                    //   storeCustomizationSetting?.dashboard?.user_email
+                    // )}
+                    label="Email"
                     name="email"
                     type="email"
                     autocomplete="username"
@@ -82,9 +83,10 @@ const ChangePassword = () => {
                 <div className="col-span-6 sm:col-span-6">
                   <InputArea
                     register={register}
-                    label={showingTranslateValue(
-                      storeCustomizationSetting?.dashboard?.current_password
-                    )}
+                    // label={showingTranslateValue(
+                    //   storeCustomizationSetting?.dashboard?.current_password
+                    // )}
+                    label="Current Password"
                     name="currentPassword"
                     type="password"
                     autocomplete="new-password"
@@ -97,9 +99,10 @@ const ChangePassword = () => {
                 <div className="col-span-6 sm:col-span-6">
                   <InputArea
                     register={register}
-                    label={showingTranslateValue(
-                      storeCustomizationSetting?.dashboard?.new_password
-                    )}
+                    // label={showingTranslateValue(
+                    //   storeCustomizationSetting?.dashboard?.new_password
+                    // )}
+                    label="New Password"
                     name="newPassword"
                     type="password"
                     autocomplete="new-password"
@@ -131,11 +134,12 @@ const ChangePassword = () => {
           ) : (
             <button
               type="submit"
-              className="md:text-sm leading-5 inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-medium text-center justify-center border-0 border-transparent rounded-md placeholder-white focus-visible:outline-none focus:outline-none bg-emerald-500 text-white px-5 md:px-6 lg:px-8 py-2 md:py-3 lg:py-3 hover:text-white hover:bg-emerald-600 h-12 mt-1 text-sm lg:text-sm w-full sm:w-auto"
+              className="md:text-sm leading-5 inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-medium text-center justify-center border-0 border-transparent rounded-md placeholder-white focus-visible:outline-none focus:outline-none bg-[#ff6b01] text-white px-5 md:px-6 lg:px-8 py-2 md:py-3 lg:py-3 hover:text-white hover:bg-black h-12 mt-1 text-base lg:text-base tracking-widest w-full sm:w-auto"
             >
-              {showingTranslateValue(
+              {/* {showingTranslateValue(
                 storeCustomizationSetting?.dashboard?.change_password
-              )}
+              )} */}
+              Update
             </button>
           )}
         </div>
