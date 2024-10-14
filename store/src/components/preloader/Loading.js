bb28f8e974cc57348ba2af9078376df793107d48
Bavneet Kaur <bavneetgulab@gmail.com>
2024-10-14 16:39:35 +0530

    changed the color of loader

--- store/src/components/preloader/Loading.js
+++ store/src/components/preloader/Loading.js
@@ -5,7 +5,7 @@ const Loading = ({ loading }) => {
   return (
     <div className="text-lg text-center py-6">
       <ScaleLoader
-        color="#34D399"
+        color="#ff6b01"
         loading={loading}
         height={30}
         width={3}
