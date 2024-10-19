ed3b38a794bb71232cb5137edcba81a7d5a7ac2e
Bavneet Kaur <bavneetgulab@gmail.com>
2024-10-19 12:46:55 +0530

    landingPage >> banner image updated

--- store/src/components/landingPage/banner.jsx
+++ store/src/components/landingPage/banner.jsx
@@ -3,20 +3,21 @@ import React from "react";
 function Banner() {
   return (
     <div className="overflow-hidden">
-       <section
+      <section
         id="home"
-        className="h-full 2xl:h-screen w-screen bg-cover bg-no-repeat object-contain bg-center"
+        className="h-full 2xl:h-screen w-screen bg-cover bg-no-repeat bg-center"
       >
-        <div className="w-full h-full">
-        <img
-          className="w-full h-auto object-cover 2xl:object-contain"  // Ensures the image covers the entire space without white gaps
-          src="/jewel/banner1.png"
-          alt="Jewellery Banner"
-        />
+        <div className="w-full h-full relative">
+          <img
+            className="w-full h-full object-cover" // Change to 'object-contain' to prevent cropping
+            src="/jewel/banner3.png"
+            alt="Jewellery Banner"
+          />
         </div>
-       
+
         {/* Responsive button positioned at the bottom-left */}
-        {/* <button className=" uppercase relative left-4 md:bottom-4 md:left-8 lg:bottom-20 lg:left-10 bg-transparent border border-2 border-[#ff6b01] text-white px-4 py-2 md:px-6 md:py-3 text-base md:text-base lg:text-lg">
+        {/* Uncomment and adjust as needed */}
+        {/* <button className="uppercase relative left-4 md:bottom-4 md:left-8 lg:bottom-20 lg:left-10 bg-transparent border border-2 border-[#ff6b01] text-white px-4 py-2 md:px-6 md:py-3 text-base md:text-base lg:text-lg">
           Shop Now
         </button> */}
       </section>
