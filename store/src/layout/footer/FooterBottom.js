d5929c32018c1d167e6e73d77cbef025ba9f5a97
Bavneet Kaur <bavneetgulab@gmail.com>
2024-10-28 10:13:27 +0530

    updated footerBottom component >> Layout

--- store/src/layout/footer/FooterBottom.js
+++ store/src/layout/footer/FooterBottom.js
@@ -6,13 +6,13 @@ const FooterBottom = () => {
     <div className="bg-white text-center py-6">
       {/* Social Media Icons */}
       <div className="flex justify-center space-x-4 mb-4">
-        <Link href="https://www.pinterest.com" target="_blank" rel="noopener noreferrer" className="bg-black hover:bg-[#ff6b01] hover:text-black text-white p-3 text-2xl rounded-full">
+        <Link href="/" target="_blank" rel="noopener noreferrer" className="bg-black hover:bg-[#ff6b01] hover:text-black text-white p-3 text-2xl rounded-full">
           <FaPinterestP />
        </Link> 
-        <Link href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="bg-black hover:bg-[#ff6b01] hover:text-black text-white p-3 text-2xl rounded-full">
+        <Link href="/" target="_blank" rel="noopener noreferrer" className="bg-black hover:bg-[#ff6b01] hover:text-black text-white p-3 text-2xl rounded-full">
           <FaFacebookF />
        </Link>
-        <Link href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="bg-black hover:bg-[#ff6b01] hover:text-black text-white p-3 text-2xl rounded-full">
+        <Link href="/" target="_blank" rel="noopener noreferrer" className="bg-black hover:bg-[#ff6b01] hover:text-black text-white p-3 text-2xl rounded-full">
           <FaInstagram />
        </Link> 
       </div>
